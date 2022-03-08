5025b17841c549485afe391bb41b490fff5d5ffa
vali <v.derhore@hotmail.fr>
2022-03-08 09:40:33 +0100

    Base du Projet

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import { Navbar } from "./components";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import icon from "../images/immosale.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={icon}></img>
+      </div>
+      <div className="nav-link">
+        <Link to="/">ImmoSale</Link>
+        <Link to="/Home">Home</Link>
+        <Link to="/Articles">Articles</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
